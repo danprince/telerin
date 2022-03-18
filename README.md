701b29d8340699dd4b185c2349a078166d74713f
Dan Prince <danprince@hey.com>
2022-03-18 21:09:31 +0100

    adds readme and doc comments

---
+++ README.md
@@ -0,0 +1,162 @@
+# Telerin
+
+Terminal/tileset renderer for web based games.
+
+* Configuration: [`constructor`](#constructor) [`codepage`](#codepage), [`scale`](#scale)
+* State: [`layer`](#layer), [`color`](#color), [`background`](#background)
+* Methods: [`clear`](#clear), [`clearLayer`](#clearLayer), [`clip`](#clip), [`put`](#put), [`write`](#write), [`box`](#box), [`refresh`](refresh), [`screenToGrid`](#screenToGrid)
+
+## Example
+
+```ts
+import { Terminal } from "telerin";
+
+let term = new Terminal(20, 20);
+term.color = "red";
+term.background = "#0000FF";
+term.put(0, 0, 0x10);
+term.write(10, 10, "Hello world");
+
+// Wait for the tileset to load before rendering
+term.tileset.onload = () => term.refresh();
+
+// Make the canvas visible
+document.body.append(term.canvas);
+```
+
+## constructor
+Creates a new terminal instance with a size in rows and columns.
+
+```ts
+new Terminal(20, 20);
+```
+
+To use a custom font/tileset pass the url and cell width/height.
+
+```ts
+new Terminal(20, 20, "mytiles.png", 16, 16);
+```
+
+## codepage
+By default the terminal uses CP437 for character mapping. Use the `codepage` property to setup an alternative map.
+
+```ts
+// Map char code 20 to char code 1
+terminal.codepage = { 20: 1 };
+```
+
+## scale
+Resize the canvas to render at a larger scale.
+
+```ts
+// Render the canvas twice as big
+terminal.scale(2);
+```
+
+## layer
+Set the current layer index.
+
+```ts
+terminal.layer = 3;
+```
+
+## color
+Sets the foreground color for subsequent drawing functions to use.
+
+```ts
+terminal.color = "red";
+terminal.color = "#FF0000";
+terminal.color = "rgb(255, 0, 0)";
+terminal.color = "hsl(0, 50%, 50%)";
+
+// Tiles will be drawn in their original colors
+terminal.color = undefined;
+```
+
+## background
+Sets the background color for subsequent drawing functions to use.
+
+```ts
+terminal.background = "red";
+terminal.background = "#FF0000";
+terminal.background = "rgb(255, 0, 0)";
+terminal.background = "hsl(0, 50%, 50%)";
+
+// Tiles will be drawn without a background color
+terminal.color = undefined;
+```
+
+## clear
+Clears the contents of all layers.
+
+```ts
+terminal.clear();
+```
+
+## clearLayer
+Clears the contents of the current layer.
+
+```ts
+terminal.clearLayer();
+```
+
+## clip
+Sets the clipping rectangle for the current layer.
+
+```ts
+// Clips content outside of a 3x4 rect at 1,2
+terminal.clip(1, 2, 3, 4)
+
+// Reset the clip area for the current layer
+terminal.clip();
+```
+
+## put
+Puts a character code into a cell.
+
+```ts
+terminal.put(0, 0, 40);
+terminal.put(0, 0, "@".charCodeAt(0));
+```
+
+Also supports passing an offset in pixels.
+
+```ts
+terminal.put(0, 0, 40, 3, -3);
+```
+
+## write
+Write a string of text.
+
+```ts
+terminal.write(0, 0, "Hello, world!");
+```
+
+Respects linebreaks, but does no wrapping.
+
+## box
+Draws a box.
+
+```ts
+// Draws a 3x4 box at 1,2 using the default box drawing characters
+terminal.write(1, 2, 3, 4);
+// Using a custom set of box drawing characters
+terminal.write(1, 2, 3, 4, "┌┐└┘─│");
+```
+
+## refresh
+Draws the contents of the terminal to the canvas.
+
+```ts
+terminal.refresh();
+```
+
+## screenToGrid
+Converts a point on the screen to terminal grid coordinates.
+
+```ts
+onmousemove = event => {
+  let { x, y } = terminal.screenToGrid(event.x, event.y);
+  // x, y are rounded grid coordinates
+}
+```
--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import defaultFontUrl from "./font.png";
 
+type TileSet = HTMLImageElement | HTMLCanvasElement;
+
+/**
+ * A clipping rect defined in terms of its top left and bottom right corners.
+ */
 interface ClipRect {
   x0: number;
   y0: number;
@@ -8,16 +13,29 @@ interface ClipRect {
 }
 
 interface Layer {
+  /**
+   * Cell data encoded into a single typed buffer. Each cell is represented
+   * with 5 sequential elements.
+   *
+   * ```
+   * [charCode, colorIndex, backgroundIndex, offsetX, offsetY]
+   * ```
+   */
   buffer: Int16Array;
+  /**
+   * The clipping rect to use when rendering this layer.
+   */
   clip: ClipRect;
 }
 
-type TileSet = HTMLImageElement | HTMLCanvasElement;
-
+let colorIndexSize = 0;
 let colorToIndex: Record<string, number> = {};
 let colorFromIndex: Record<number, string> = {};
-let colorIndexSize = 0;
 
+/**
+ * Converts a color to an index value that can be stored in a layer
+ * buffer.
+ */
 function getColorIndex(color: string | undefined): number {
   if (!color) return 0;
   if (color in colorToIndex) return colorToIndex[color];
@@ -26,23 +44,69 @@ function getColorIndex(color: string | undefined): number {
   return colorToIndex[color] = index;
 }
 
+/**
+ * Mapping to convert character codes into tileset indexes.
+ */
 export type CodePage = Record<number, number>;
+
 export const CP437: CodePage = { 9786: 1, 9787: 2, 9829: 3, 9830: 4, 9827: 5, 9824: 6, 8226: 7, 9688: 8, 9675: 9, 9689: 10, 9794: 11, 9792: 12, 9834: 13, 9835: 14, 9788: 15, 9658: 16, 9668: 17, 8597: 18, 8252: 19, 182: 20, 167: 21, 9644: 22, 8616: 23, 8593: 24, 8595: 25, 8594: 26, 8592: 27, 8735: 28, 8596: 29, 9650: 30, 9660: 31, 8962: 127, 199: 128, 252: 129, 233: 130, 226: 131, 228: 132, 224: 133, 229: 134, 231: 135, 234: 136, 235: 137, 232: 138, 239: 139, 238: 140, 236: 141, 196: 142, 197: 143, 201: 144, 230: 145, 198: 146, 244: 147, 246: 148, 242: 149, 251: 150, 249: 151, 255: 152, 214: 153, 220: 154, 162: 155, 163: 156, 165: 157, 8359: 158, 402: 159, 225: 160, 237: 161, 243: 162, 250: 163, 241: 164, 209: 165, 170: 166, 186: 167, 191: 168, 8976: 169, 172: 170, 189: 171, 188: 172, 161: 173, 171: 174, 187: 175, 9617: 176, 9618: 177, 9619: 178, 9474: 179, 9508: 180, 9569: 181, 9570: 182, 9558: 183, 9557: 184, 9571: 185, 9553: 186, 9559: 187, 9565: 188, 9564: 189, 9563: 190, 9488: 191, 9492: 192, 9524: 193, 9516: 194, 9500: 195, 9472: 196, 9532: 197, 9566: 198, 9567: 199, 9562: 200, 9556: 201, 9577: 202, 9574: 203, 9568: 204, 9552: 205, 9580: 206, 9575: 207, 9576: 208, 9572: 209, 9573: 210, 9561: 211, 9560: 212, 9554: 213, 9555: 214, 9579: 215, 9578: 216, 9496: 217, 9484: 218, 9608: 219, 9604: 220, 9612: 221, 9616: 222, 9600: 223, 945: 224, 223: 225, 915: 226, 960: 227, 931: 228, 963: 229, 181: 230, 964: 231, 934: 232, 920: 233, 937: 234, 948: 235, 8734: 236, 966: 237, 949: 238, 8745: 239, 8801: 240, 177: 241, 8805: 242, 8804: 243, 8992: 244, 8993: 245, 247: 246, 8776: 247, 176: 248, 8729: 249, 183: 250, 8730: 251, 8319: 252, 178: 253, 9632: 254 };
 
 export class Terminal {
+  /**
+   * Width in cells.
+   */
   readonly width: number;
+  /**
+   * Height in cells.
+   */
   readonly height: number;
+  /**
+   * Canvas that the terminal renders to.
+   */
   readonly canvas: HTMLCanvasElement;
+
   readonly ctx: CanvasRenderingContext2D;
+  /**
+   * Image/canvas that contains the tileset.
+   */
   readonly tileset: TileSet;
+  /**
+   * Width of cells in pixels.
+   */
   readonly cellWidth: number;
+  /**
+   * Height of cells in pixels.
+   */
   readonly cellHeight: number;
+  /**
+   * List of layers in the terminal.
+   */
   private layers: Layer[] = [];
+  /**
+   * A cache of tinted versions of the tileset.
+   */
   private tints: Record<string, HTMLCanvasElement> = {};
 
+  /**
+   * Current foreground color that will be used in draw calls.
+   *
+   * Set to `undefined` to draw cells without tinting.
+   */
   color: string | undefined;
+  /**
+   * Current background color that will be used in draw calls.
+   *
+   * Set to `undefined` to skip rendering background colors.
+   */
   background: string | undefined;
+  /**
+   * Current layer (defaults to 0) to that the other drawing functions will operate on.
+   * Must be a positive integer.
+   */
   layer: number = 0;
+  /**
+   * The current {@link CodePage}. Defaults to {@link CP437}.
+   */
   codepage: CodePage = CP437;
 
   constructor(
@@ -69,6 +133,9 @@ export class Terminal {
     this.cellHeight = cellHeight
   }
 
+  /**
+   * Creates a layer with an empty buffer and the default clipping rect.
+   */
   private createLayer() {
     return {
       buffer: new Int16Array(this.width * this.height * 5),
@@ -76,11 +143,17 @@ export class Terminal {
     };
   }
 
-  private getLayer() {
+  /**
+   * Gets the current layer and creates it if it doesn't exist.
+   */
+  private getLayer(): Layer {
     return this.layers[this.layer] ||
       (this.layers[this.layer] = this.createLayer());
   }
 
+  /**
+   * Creates a color tinted version of the tileset.
+   */
   tint(color: string): TileSet {
     let tinted = this.tints[color];
     if (tinted) return tinted;
@@ -93,6 +166,22 @@ export class Terminal {
     return this.tints[color] = canvas;
   }
 
+  /**
+   * Set the scale factor for the canvas.
+   * 
+   * ```ts
+   * terminal.scale(2)
+   * ```
+   */
+  scale(factor: number) {
+    let { canvas } = this;
+    canvas.style.width = canvas.width * factor + "px";
+    canvas.style.height = canvas.height * factor + "px";
+  }
+
+  /**
+   * Clears the contents of all layers.
+   */
   clear() {
     for (let layer of this.layers) {
       if (layer) {
@@ -101,14 +190,46 @@ export class Terminal {
     }
   }
 
+  /**
+   * Clears the contents of the current layer.
+   */
   clearLayer() {
     this.getLayer().buffer.fill(0);
   }
 
+  /**
+   * Set the clipping rectangle for the current layer.
+   *
+   * ```ts
+   * terminal.clip(0, 0, 10, 10);
+   * ```
+   *
+   * Call with no arguments to reset the clipping rectangle.
+   * 
+   * ```ts
+   * terminal.clip();
+   * ```
+   *
+   * The clipping rect is only applied when rendering.
+   */
   clip(x = 0, y = 0, width = this.width, height = this.height) {
     this.getLayer().clip = { x0: x, y0: y, x1: x + width, y1: y + height };
   }
 
+  /**
+   * Put a character into a cell.
+   *
+   * ```ts
+   * terminal.put(0, 0, 0x40)
+   * terminal.put(0, 0, 0x40, -1, -1)
+   * ```
+   *
+   * @param x X cell coordinate
+   * @param y Y cell coordinate
+   * @param code Character code
+   * @param offsetX Horizontal offset in pixels
+   * @param offsetY Vertical offset in pixels
+   */
   put(x: number, y: number, code: number, offsetX = 0, offsetY = 0) {
     let { width, height, color, background, codepage } = this;
     if (x < 0 || y < 0 || x >= width || y >= height) return;
@@ -121,6 +242,9 @@ export class Terminal {
     buffer[i] = offsetY;
   }
 
+  /**
+   * Renders the terminal to the canvas.
+   */
   refresh() {
     let { canvas, ctx, tileset: font, width, cellWidth, cellHeight } = this;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -154,12 +278,13 @@ export class Terminal {
     }
   }
 
-  scale(factor: number) {
-    let { canvas } = this;
-    canvas.style.width = canvas.width * factor + "px";
-    canvas.style.height = canvas.height * factor + "px";
-  }
-
+  /**
+   * Writes a string of text to the current layer.
+   *
+   * ```ts
+   * terminal.write(1, 2, "Hello, world!")
+   * ```
+   */
   write(x: number, y: number, text: string) {
     let tx = x;
     let ty = y;
@@ -173,6 +298,17 @@ export class Terminal {
     }
   }
 
+  /**
+   * Draw a box.
+   *
+   * ```ts
+   * // Use the default box drawing characters
+   * terminal.box(0, 0, 5, 5);
+   * 
+   * // Use custom box drawing characters
+   * terminal.box(0, 0, 5, 5, "┌┐└┘─│");
+   * ```
+   */
   box(x: number, y: number, w: number, h: number, chars = "┌┐└┘─│") {
     let [x0, y0, x1, y1] = [x, y, x + w - 1, y + h - 1];
     this.put(x0, y0, chars.charCodeAt(0));
@@ -191,6 +327,10 @@ export class Terminal {
     }
   }
 
+  /**
+   * Converts coordinates in screen space (e.g. `event.clientX` and
+   * `event.clientY`) to rounded grid coordinates.
+   */
   screenToGrid(x: number, y: number): { x: number, y: number } {
     let { width, height, cellWidth, cellHeight, canvas } = this;
     let rect = canvas.getBoundingClientRect();
