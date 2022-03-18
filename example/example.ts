a23c3b6604e32f3f9280cdb186227ed43ea18a11
Dan Prince <danprince@hey.com>
2022-03-18 19:31:06 +0100

    initial commit

--- example/example.ts
+++ example/example.ts
@@ -1,20 +1,5 @@
 import { Terminal } from "..";
 
-let t = new Terminal(30, 30);
-let m = { x: 0, y: 0 };
-
-t.canvas.style.width = t.canvas.width * 3 + "px";
-t.canvas.style.height = t.canvas.height * 3 + "px";
-document.body.append(t.canvas);
-
-t.ready().then(() => {
-  update();
-  onmousemove = event => {
-    m = t.screenToGrid(event.clientX, event.clientY);
-    update();
-  };
-});
-
 const BOX_DRAWING_EXAMPLE = `
 ┌─┬┐  ╔═╦╗  ╓─╥╖  ╒═╤╕
 │ ││  ║ ║║  ║ ║║  │ ││
@@ -30,9 +15,53 @@ const BOX_DRAWING_EXAMPLE = `
  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 `;
 
-function update() {
-  t.clear();
-  t.write(1, 1, BOX_DRAWING_EXAMPLE, "blue");
-  t.put(m.x, m.y, 0x40, "black", "white");
+
+let t = new Terminal(30, 30);
+t.scale(2);
+let m = { x: 0, y: 0 };
+
+onmousemove = e => {
+  m = t.screenToGrid(e.clientX, e.clientY);
 }
 
+let code = "";
+fetch("example.js").then(res => res.text()).then(t => code = t);
+
+function start() {
+  drawBackground();
+  loop();
+}
+
+function drawBackground() {
+  t.layer = 1;
+  for (let x = 0; x < t.width; x++) {
+    for (let y = 0; y < t.height; y++) {
+      let code = Math.random() * 400 | 0;
+      let offsetX = (Math.random() - 0.5) * 10;
+      let offsetY = (Math.random() - 0.5) * 10;
+      let h = Math.random() * 360 | 0;
+      t.color = `hsl(${h}, 80%, 60%)`;
+      t.put(x, y, code, offsetX, offsetY);
+    }
+  }
+
+  t.write(0, 0, BOX_DRAWING_EXAMPLE);
+}
+
+function loop() {
+  requestAnimationFrame(loop);
+
+  t.layer = 3;
+  t.clearLayer();
+  t.color = "white";
+  t.background = "black";
+  t.box(m.x, m.y, 3, 3, "▒▒▒▒▒▒▒");
+  t.box(m.x - 1, m.y - 1, 3, 3);
+  t.put(m.x, m.y, "X".charCodeAt(0));
+
+  t.refresh();
+}
+
+t.tileset.onload = start;
+t.canvas.style.background = "black";
+document.body.append(t.canvas);
--- index.ts
+++ index.ts
@@ -1,99 +1,166 @@
-import defaultFontSrc from "./font.png";
+import defaultFontUrl from "./font.png";
 
-export interface Font {
-  image: HTMLImageElement;
-  charWidth: number;
-  charHeight: number;
+interface ClipRect {
+  x0: number;
+  y0: number;
+  x1: number;
+  y1: number;
 }
 
-const defaultFontImage = new Image();
-defaultFontImage.src = defaultFontSrc;
+interface Layer {
+  buffer: Int16Array;
+  clip: ClipRect;
+}
 
-const defaultFont: Font = {
-  image: defaultFontImage,
-  charWidth: 6,
-  charHeight: 6,
-};
+type TileSet = HTMLImageElement | HTMLCanvasElement;
 
-type CodePage = Record<number, number>;
+let colorToIndex: Record<string, number> = {};
+let colorFromIndex: Record<number, string> = {};
+let colorIndexSize = 0;
+
+function getColorIndex(color: string | undefined): number {
+  if (!color) return 0;
+  if (color in colorToIndex) return colorToIndex[color];
+  let index = ++colorIndexSize;
+  colorFromIndex[index] = color;
+  return colorToIndex[color] = index;
+}
+
+export type CodePage = Record<number, number>;
 export const CP437: CodePage = { 9786: 1, 9787: 2, 9829: 3, 9830: 4, 9827: 5, 9824: 6, 8226: 7, 9688: 8, 9675: 9, 9689: 10, 9794: 11, 9792: 12, 9834: 13, 9835: 14, 9788: 15, 9658: 16, 9668: 17, 8597: 18, 8252: 19, 182: 20, 167: 21, 9644: 22, 8616: 23, 8593: 24, 8595: 25, 8594: 26, 8592: 27, 8735: 28, 8596: 29, 9650: 30, 9660: 31, 8962: 127, 199: 128, 252: 129, 233: 130, 226: 131, 228: 132, 224: 133, 229: 134, 231: 135, 234: 136, 235: 137, 232: 138, 239: 139, 238: 140, 236: 141, 196: 142, 197: 143, 201: 144, 230: 145, 198: 146, 244: 147, 246: 148, 242: 149, 251: 150, 249: 151, 255: 152, 214: 153, 220: 154, 162: 155, 163: 156, 165: 157, 8359: 158, 402: 159, 225: 160, 237: 161, 243: 162, 250: 163, 241: 164, 209: 165, 170: 166, 186: 167, 191: 168, 8976: 169, 172: 170, 189: 171, 188: 172, 161: 173, 171: 174, 187: 175, 9617: 176, 9618: 177, 9619: 178, 9474: 179, 9508: 180, 9569: 181, 9570: 182, 9558: 183, 9557: 184, 9571: 185, 9553: 186, 9559: 187, 9565: 188, 9564: 189, 9563: 190, 9488: 191, 9492: 192, 9524: 193, 9516: 194, 9500: 195, 9472: 196, 9532: 197, 9566: 198, 9567: 199, 9562: 200, 9556: 201, 9577: 202, 9574: 203, 9568: 204, 9552: 205, 9580: 206, 9575: 207, 9576: 208, 9572: 209, 9573: 210, 9561: 211, 9560: 212, 9554: 213, 9555: 214, 9579: 215, 9578: 216, 9496: 217, 9484: 218, 9608: 219, 9604: 220, 9612: 221, 9616: 222, 9600: 223, 945: 224, 223: 225, 915: 226, 960: 227, 931: 228, 963: 229, 181: 230, 964: 231, 934: 232, 920: 233, 937: 234, 948: 235, 8734: 236, 966: 237, 949: 238, 8745: 239, 8801: 240, 177: 241, 8805: 242, 8804: 243, 8992: 244, 8993: 245, 247: 246, 8776: 247, 176: 248, 8729: 249, 183: 250, 8730: 251, 8319: 252, 178: 253, 9632: 254 };
 
 export class Terminal {
-  width: number;
-  height: number;
-  canvas: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D;
+  readonly width: number;
+  readonly height: number;
+  readonly canvas: HTMLCanvasElement;
+  readonly ctx: CanvasRenderingContext2D;
+  readonly tileset: TileSet;
+  readonly cellWidth: number;
+  readonly cellHeight: number;
+  private layers: Layer[] = [];
+  private tints: Record<string, HTMLCanvasElement> = {};
+
+  color: string | undefined;
+  background: string | undefined;
+  layer: number = 0;
   codepage: CodePage = CP437;
 
-  private font: Font;
-  private tintCache = new Map<string, HTMLCanvasElement>();
-
-  constructor(width: number, height: number, font: Font = defaultFont) {
+  constructor(
+    width: number,
+    height: number,
+    url: string = defaultFontUrl,
+    cellWidth = 6,
+    cellHeight = 6,
+  ) {
+    let tileset = new Image();
+    tileset.src = url;
     let canvas = document.createElement("canvas");
     let ctx = canvas.getContext("2d")!;
-    canvas.width = width * font.charWidth;
-    canvas.height = height * font.charHeight;
-    canvas.style.imageRendering = "pixelated";
+    canvas.width = width * cellWidth;
+    canvas.height = height * cellHeight;
     ctx.imageSmoothingEnabled = false;
-
+    canvas.style.imageRendering = "pixelated";
+    this.tileset = tileset;
     this.width = width;
     this.height = height;
-    this.font = font;
     this.canvas = canvas;
     this.ctx = ctx;
+    this.cellWidth = cellWidth;
+    this.cellHeight = cellHeight
   }
 
-  private tint(color: string) {
-    let cached = this.tintCache.get(color);
-    if (cached) return cached;
+  private createLayer() {
+    return {
+      buffer: new Int16Array(this.width * this.height * 5),
+      clip: { x0: 0, y0: 0, x1: this.width - 1, y1: this.height - 1 },
+    };
+  }
 
+  private getLayer() {
+    return this.layers[this.layer] ||
+      (this.layers[this.layer] = this.createLayer());
+  }
+
+  tint(color: string): TileSet {
+    let tinted = this.tints[color];
+    if (tinted) return tinted;
     let canvas = document.createElement("canvas");
     let ctx = canvas.getContext("2d")!;
-    canvas.width = this.font.image.width;
-    canvas.height = this.font.image.height;
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(this.font.image, 0, 0);
-    ctx.fillStyle = color;
+    ctx.drawImage(this.tileset, 0, 0);
     ctx.globalCompositeOperation = "source-atop";
+    ctx.fillStyle = color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    this.tintCache.set(color, canvas);
-    return canvas;
+    return this.tints[color] = canvas;
   }
 
-  ready() {
-    return new Promise<void>((resolve, reject) => {
-      this.font.image.addEventListener("load", () => resolve());
-      this.font.image.addEventListener("error", reject);
-    });
-  }
-
-  put(x: number, y: number, code: number, fg?: string, bg?: string) {
-    // If the image hasn't loaded yet, we need to bail out of rendering
-    if (this.font.image.width === 0 || this.font.image.height === 0) return;
-
-    code = this.codepage[code] ?? code;
-    let img: HTMLImageElement | HTMLCanvasElement = this.font.image;
-    let cw = this.font.charWidth;
-    let ch = this.font.charHeight;
-    let cols = img.width / cw;
-    let sx = (code % cols) * cw;
-    let sy = (code / cols | 0) * ch;
-    let dx = x * cw;
-    let dy = y * ch;
-
-    if (fg) {
-      img = this.tint(fg);
+  clear() {
+    for (let layer of this.layers) {
+      if (layer) {
+        layer.buffer.fill(0);
+      }
     }
-
-    if (bg) {
-      this.ctx.fillStyle = bg;
-      this.ctx.fillRect(dx, dy, cw, ch);
-    }
-
-    this.ctx.drawImage(img, sx, sy, cw, ch, dx, dy, cw, ch);
   }
 
-  write(x: number, y: number, text: string, fg?: string, bg?: string) {
+  clearLayer() {
+    this.getLayer().buffer.fill(0);
+  }
+
+  clip(x = 0, y = 0, width = this.width, height = this.height) {
+    this.getLayer().clip = { x0: x, y0: y, x1: x + width, y1: y + height };
+  }
+
+  put(x: number, y: number, code: number, offsetX = 0, offsetY = 0) {
+    let { width, height, color, background, codepage } = this;
+    if (x < 0 || y < 0 || x >= width || y >= height) return;
+    let { buffer } = this.getLayer();
+    let i = (x + y * width) * 5;
+    buffer[i++] = codepage[code] || code;
+    buffer[i++] = getColorIndex(color);
+    buffer[i++] = getColorIndex(background);
+    buffer[i++] = offsetX;
+    buffer[i] = offsetY;
+  }
+
+  refresh() {
+    let { canvas, ctx, tileset: font, width, cellWidth, cellHeight } = this;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let layer of this.layers) {
+      if (layer == null) continue;
+      let { buffer, clip } = layer;
+      for (let y = clip.y0; y <= clip.y1; y++) {
+        for (let x = clip.x0; x <= clip.x1; x++) {
+          let i = (x + y * width) * 5;
+          let code = buffer[i++];
+          if (!code) continue;
+          let fg = buffer[i++];
+          let bg = buffer[i++];
+          let offsetX = buffer[i++];
+          let offsetY = buffer[i++];
+          let img: HTMLImageElement | HTMLCanvasElement = font;
+          let cols = font.width / cellWidth;
+          let cw = cellWidth;
+          let ch = cellHeight;
+          let sx = (code % cols) * cw;
+          let sy = (code / cols | 0) * ch;
+          let dx = x * cw + offsetX;
+          let dy = y * ch + offsetY;
+          if (fg) img = this.tint(colorFromIndex[fg]);
+          if (bg) ctx.fillStyle = colorFromIndex[bg];
+          if (bg) ctx.fillRect(dx, dy, cw, ch);
+          ctx.drawImage(img, sx, sy, cw, ch, dx, dy, cw, ch);
+        }
+      }
+    }
+  }
+
+  scale(factor: number) {
+    let { canvas } = this;
+    canvas.style.width = canvas.width * factor + "px";
+    canvas.style.height = canvas.height * factor + "px";
+  }
+
+  write(x: number, y: number, text: string) {
     let tx = x;
     let ty = y;
     for (let i = 0; i < text.length; i++) {
@@ -101,29 +168,36 @@ export class Terminal {
         tx = x;
         ty++;
       } else {
-        this.put(tx++, ty, text.charCodeAt(i), fg, bg);
+        this.put(tx++, ty, text.charCodeAt(i));
       }
     }
   }
 
-  clear(
-    x: number = 0,
-    y: number = 0,
-    width: number = this.width,
-    height: number = this.height
-  ) {
-    let cw = this.font.charWidth;
-    let ch = this.font.charHeight;
-    this.ctx.clearRect(x * cw, y * ch, width * cw, height * ch);
+  box(x: number, y: number, w: number, h: number, chars = "┌┐└┘─│") {
+    let [x0, y0, x1, y1] = [x, y, x + w - 1, y + h - 1];
+    this.put(x0, y0, chars.charCodeAt(0));
+    this.put(x1, y0, chars.charCodeAt(1));
+    this.put(x0, y1, chars.charCodeAt(2));
+    this.put(x1, y1, chars.charCodeAt(3));
+
+    for (let x = x0 + 1; x < x1; x++) {
+      this.put(x, y0, chars.charCodeAt(4));
+      this.put(x, y1, chars.charCodeAt(4));
+    }
+
+    for (let y = y0 + 1; y < y1; y++) {
+      this.put(x0, y, chars.charCodeAt(5));
+      this.put(x1, y, chars.charCodeAt(5));
+    }
   }
 
-  screenToGrid(x: number, y: number) {
-    let { charWidth, charHeight } = this.font;
-    let rect = this.canvas.getBoundingClientRect();
-    let scaleX = rect.width / charWidth / this.width;
-    let scaleY = rect.height / charHeight / this.height;
-    let gridX = (x - rect.x) / charWidth / scaleX;
-    let gridY = (y - rect.y) / charHeight / scaleY;
+  screenToGrid(x: number, y: number): { x: number, y: number } {
+    let { width, height, cellWidth, cellHeight, canvas } = this;
+    let rect = canvas.getBoundingClientRect();
+    let scaleX = rect.width / cellWidth / width;
+    let scaleY = rect.height / cellHeight / height;
+    let gridX = (x - rect.x) / cellWidth / scaleX;
+    let gridY = (y - rect.y) / cellHeight / scaleY;
     return { x: Math.floor(gridX), y: Math.floor(gridY) };
   }
 }
